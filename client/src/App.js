2e94698f126a1c1a8df13567260d929722ae8ef1
thakursachin467 <thakursachin467@gmail.com>
2018-09-03 22:42:48 +0530

    Added UI for Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Layouts/Navbar'
+import Footer from './components/Layouts/Footer'
+import Landing from './components/Layouts/Landing'
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>DevConnect</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/login" component={Login}></Route>
+            <Route exact path="/register" component={Register}></Route>
+          </div>
+
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/Layouts/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <div>
+            <footer className="footer bg-dark text-white  text-center">
+                <div className="container">
+                    <span className="text-muted">Copyrights &copy; {new Date().getFullYear()} DevConnect</span>
+                </div>
+
+            </footer>
+        </div>
+    )
+}
